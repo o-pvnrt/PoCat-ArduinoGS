49509083a75b7ca7c132ca95a2a7f472962116a7
Oscar <oscarpavon2003@gmail.com>
2025-05-05 13:47:46 +0200

    Doxygen-Type Documentation

--- PoCat_LoRaArduinoGS/PoCat_LoRaArduinoGS.ino
+++ PoCat_LoRaArduinoGS/PoCat_LoRaArduinoGS.ino
@@ -1,86 +1,192 @@
-
+/**
+ * @file PoCat_LoRaArduinoGS.ino
+ * @brief Main application for LoRa communication with telecommand handling and interleaving.
+ *
+ * This file implements the main logic for sending and receiving telecommands over LoRa,
+ * including packet interleaving, command handling, and serial interface.
+ */
 #include "LoRaWan_APP.h"
 #include "Arduino.h"
 #ifndef LoraWan_RGB
 #define LoraWan_RGB 0
 #endif
+
+/**
+ * @def RF_FREQUENCY
+ * @brief LoRa radio frequency in Hz.
+ */
 #define RF_FREQUENCY                                868000000 // Hz
+/**
+ * @def TX_OUTPUT_POWER
+ * @brief LoRa transmit output power in dBm.
+ */
 #define TX_OUTPUT_POWER                             10        // dBm
+/**
+ * @def LORA_BANDWIDTH
+ * @brief LoRa bandwidth setting.
+ * 0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved
+ */
 #define LORA_BANDWIDTH                              0         // [0: 125 kHz,
                                                               //  1: 250 kHz,
                                                               //  2: 500 kHz,
                                                               //  3: Reserved]
+/**
+ * @def LORA_SPREADING_FACTOR
+ * @brief LoRa spreading factor (SF7..SF12).
+ */
 #define LORA_SPREADING_FACTOR                       11        // [SF7..SF12]
+/**
+ * @def LORA_CODINGRATE
+ * @brief LoRa coding rate (1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8).
+ */
 #define LORA_CODINGRATE                             1         // [1: 4/5,
                                                               //  2: 4/6,
                                                               //  3: 4/7,
                                                               //  4: 4/8]
+/**
+ * @def LORA_PREAMBLE_LENGTH
+ * @brief LoRa preamble length for Tx and Rx.
+ */
 #define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+/**
+ * @def LORA_SYMBOL_TIMEOUT
+ * @brief LoRa symbol timeout in symbols.
+ */
 #define LORA_SYMBOL_TIMEOUT                         100         // Symbols
+/**
+ * @def LORA_FIX_LENGTH_PAYLOAD_ON
+ * @brief LoRa fixed length payload mode.
+ */
 #define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+/**
+ * @def LORA_IQ_INVERSION_ON
+ * @brief LoRa IQ inversion mode.
+ */
 #define LORA_IQ_INVERSION_ON                        false
+/**
+ * @def RX_TIMEOUT_VALUE
+ * @brief LoRa receive timeout value in ms.
+ */
 #define RX_TIMEOUT_VALUE                            4000
+/**
+ * @def BUFFER_SIZE
+ * @brief Payload buffer size.
+ */
 #define BUFFER_SIZE                                 100 // Define the payload size here
 
-
+/**
+ * @enum telecommandIDS
+ * @brief Telecommand identifiers for various system operations.
+ */
 typedef enum {
-    ERR,
-    PING,
-    TRANSIT_TO_NM,
-    TRANSIT_TO_CM,
-    TRANSIT_TO_SSM,
-    TRANSIT_TO_SM,
-    UPLOAD_ADCS_CALIBRATION,
-    UPLOAD_ADCS_TLE,
-    UPLOAD_COMMS_CONFIG,
-    COMMS_UPLOAD_PARAMS,
-    UPLOAD_UNIX_TIME,
-    UPLOAD_EPS_TH,
-    UPLOAD_PL_CONFIG,
-    DOWNLINK_CONFIG,
-    EPS_HEATER_ENABLE,
-    EPS_HEATER_DISABLE,
-    POL_PAYLOAD_SHUT,
-    POL_ADCS_SHUT,
-    POL_BURNCOMMS_SHUT,
-    POL_HEATER_SHUT,
-    POL_PAYLOAD_ENABLE,
-    POL_ADCS_ENABLE,
-    POL_BURNCOMMS_ENABLE,
-    POL_HEATER_ENABLE,
-    CLEAR_PL_DATA,
-    CLEAR_FLASH,
-    CLEAR_HT,
-    COMMS_STOP_TX,
-    COMMS_RESUME_TX,
-    COMMS_IT_DOWNLINK,
-    COMMS_HT_DOWNLINK,
-    PAYLOAD_SCHEDULE,
-    PAYLOAD_DEACTIVATE,
-    PAYLOAD_SEND_DATA,
-    OBC_HARD_REBOOT,
-    OBC_SOFT_REBOOT,
-    OBC_PERIPH_REBOOT,
-    OBC_DEBUG_MODE
+    ERR,                /**< Error */
+    PING,               /**< Ping */
+    TRANSIT_TO_NM,      /**< Transit to NM */
+    TRANSIT_TO_CM,      /**< Transit to CM */
+    TRANSIT_TO_SSM,     /**< Transit to SSM */
+    TRANSIT_TO_SM,      /**< Transit to SM */
+    UPLOAD_ADCS_CALIBRATION, /**< Upload ADCS calibration */
+    UPLOAD_ADCS_TLE,    /**< Upload ADCS TLE */
+    UPLOAD_COMMS_CONFIG,/**< Upload comms config */
+    COMMS_UPLOAD_PARAMS,/**< Comms upload params */
+    UPLOAD_UNIX_TIME,   /**< Upload UNIX time */
+    UPLOAD_EPS_TH,      /**< Upload EPS thresholds */
+    UPLOAD_PL_CONFIG,   /**< Upload payload config */
+    DOWNLINK_CONFIG,    /**< Downlink config */
+    EPS_HEATER_ENABLE,  /**< Enable EPS heater */
+    EPS_HEATER_DISABLE, /**< Disable EPS heater */
+    POL_PAYLOAD_SHUT,   /**< Shut down payload */
+    POL_ADCS_SHUT,      /**< Shut down ADCS */
+    POL_BURNCOMMS_SHUT, /**< Shut down burn comms */
+    POL_HEATER_SHUT,    /**< Shut down heater */
+    POL_PAYLOAD_ENABLE, /**< Enable payload */
+    POL_ADCS_ENABLE,    /**< Enable ADCS */
+    POL_BURNCOMMS_ENABLE,/**< Enable burn comms */
+    POL_HEATER_ENABLE,  /**< Enable heater */
+    CLEAR_PL_DATA,      /**< Clear payload data */
+    CLEAR_FLASH,        /**< Clear flash memory */
+    CLEAR_HT,           /**< Clear housekeeping telemetry */
+    COMMS_STOP_TX,      /**< Stop comms transmission */
+    COMMS_RESUME_TX,    /**< Resume comms transmission */
+    COMMS_IT_DOWNLINK,  /**< Initiate comms downlink */
+    COMMS_HT_DOWNLINK,  /**< Initiate housekeeping telemetry downlink */
+    PAYLOAD_SCHEDULE,   /**< Schedule payload */
+    PAYLOAD_DEACTIVATE, /**< Deactivate payload */
+    PAYLOAD_SEND_DATA,  /**< Send payload data */
+    OBC_HARD_REBOOT,    /**< Hard reboot of OBC */
+    OBC_SOFT_REBOOT,    /**< Soft reboot of OBC */
+    OBC_PERIPH_REBOOT,  /**< Reboot peripherals of OBC */
+    OBC_DEBUG_MODE      /**< Enable debug mode */
 } telecommandIDS;
 
+/**
+ * @var txpacket
+ * @brief Buffer for outgoing packets.
+ */
 uint8_t txpacket[48];
+/**
+ * @var RxData
+ * @brief Buffer for received data.
+ */
 uint8_t RxData[48];
 
+/**
+ * @var RadioEvents
+ * @brief Structure holding radio event callbacks.
+ */
 static RadioEvents_t RadioEvents;
 
+/**
+ * @var rssi
+ * @brief Received signal strength indicator.
+ */
 int16_t rssi,rxSize;
+/**
+ * @var tcinput
+ * @brief String for telecommand input from serial.
+ */
 String tcinput;
+/**
+ * @var tcnumber
+ * @brief Telecommand number parsed from input.
+ */
 int tcnumber=0;
+/**
+ * @var i
+ * @brief General purpose index, used in loops.
+ */
 volatile int i=0;
+/**
+ * @var lora_idle
+ * @brief Indicates if LoRa is idle and ready for new operation.
+ */
 bool lora_idle = true;
+/**
+ * @var senddata_TLC_sent
+ * @brief Counter for sent TLC data.
+ */
 int senddata_TLC_sent=0;
 
+/**
+ * @var tcpacket
+ * @brief Buffer for telecommand packet.
+ */
 uint8_t tcpacket[48]={0xC8,0x9D,0x00,0x00,0x00,0x00,0x03,0x83,0x1E,0x19,0xDC,0x63,0x53,0xC4};
+/**
+ * @var Encoded_Packet
+ * @brief Buffer for encoded (interleaved) packet.
+ */
 uint8_t Encoded_Packet[48];
 
+/**
+ * @var totalpacketsize
+ * @brief Size of the packet to be sent.
+ */
 uint8_t totalpacketsize=0;
 
+/**
+ * @brief Cubecell setup function. Initializes serial, radio, and event handlers.
+ */
 void setup() {
     Serial.begin(115200);
     rssi=0;
@@ -101,7 +207,9 @@ void setup() {
     RadioEvents.RxDone = OnRxDone;
    }
 
-
+/**
+ * @brief Cubecell main loop. Handles serial input and LoRa idle state.
+ */
 void loop()
 {
   tcnumber=0;
@@ -123,7 +231,10 @@ void loop()
   }
   }
 
-
+/**
+ * @brief Sends a telecommand packet over LoRa.
+ * @param TC Telecommand ID to send.
+ */
 void SendTC(uint8_t TC) {
   tcpacket[2]=TC;
   switch (TC) {
@@ -281,11 +392,23 @@ void SendTC(uint8_t TC) {
   Radio.Send(Encoded_Packet,sizeof(Encoded_Packet));
 }
 
+/**
+ * @brief Prints a byte as two-digit hexadecimal to Serial.
+ * @param num Byte to print.
+ */
 void printHex(uint8_t num) {
   char hexCar[2];
   sprintf(hexCar, "%02X", num);
   Serial.print(hexCar);
 }
+
+/**
+ * @brief Callback for when a LoRa packet is received.
+ * @param payload Pointer to received data.
+ * @param size Size of received data.
+ * @param rssi Received signal strength indicator.
+ * @param snr Signal-to-noise ratio.
+ */
 void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
 {
     turnOnRGB(COLOR_RECEIVED,0);
@@ -310,12 +433,20 @@ void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
     lora_idle = true;
 }
 
+/**
+ * @brief Callback for when a LoRa packet is sent.
+ */
 void OnTxDone()
 {
   lora_idle = true;;
   
 }
 
+/**
+ * @brief Interleaves the input array in groups of 6 for error resilience.
+ * @param inputarr Pointer to input array.
+ * @param size Size of the array (must be multiple of 6).
+ */
 void interleave(uint8_t *inputarr, int size) {
     // Check that the size is a multiple of 6.
     if (size % 6 != 0) {
@@ -344,6 +475,11 @@ void interleave(uint8_t *inputarr, int size) {
     free(temp);
 }
 
+/**
+ * @brief Deinterleaves the input array in groups of 6 to restore original order.
+ * @param inputarr Pointer to input array.
+ * @param size Size of the array (must be multiple of 6).
+ */
 void deinterleave(uint8_t *inputarr, int size) {
     if (size % 6 != 0) {
         return;
